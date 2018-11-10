1d55a5a6b50d7d7aea32fad8a4375893190b9bd6
Igor Escobar <igor.escobar-ext@man.eu>
2018-11-10 21:43:01 +0000

    fixing uninstall hook

--- imageboss.php
+++ imageboss.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: ImageBoss
 Description: Content aware image resizing, cropping, compression, cache and CDN. All web development best practices, hassle free in one simple and powerful API.
-Version: 1.0.9
+Version: 1.0.11
 Author: ImageBoss
 Author URI: https://imageboss.me
 License: MIT
