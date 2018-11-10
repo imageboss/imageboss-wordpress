7a969e74cd2f088eef3363a7f3c48ea623ba8953
Igor Escobar <igor.escobar-ext@man.eu>
2018-11-10 00:28:13 +0000

    new release

--- deploy.sh
+++ deploy.sh
@@ -1,239 +1,86 @@
 #! /bin/bash
-# See https://github.com/GaryJones/wordpress-plugin-svn-deploy for instructions and credits.
-#
-# Steps to deploying:
-#
-#  1. Ask for plugin slug.
-#  2. Ask for local plugin directory.
-#  3. Check local plugin directory exists.
-#  4. Ask for main plugin file name.
-#  5. Check main plugin file exists.
-#  6. Check readme.txt version matches main plugin file version.
-#  7. Ask for temporary SVN path.
-#  8. Ask for remote SVN repo.
-#  9. Ask for SVN username.
-# 10. Ask if input is correct, and give chance to abort.
-# 11. Check if Git tag exists for version number (must match exactly).
-# 12. Checkout SVN repo.
-# 13. Set to SVN ignore some GitHub-related files.
-# 14. Export HEAD of master from git to the trunk of SVN.
-# 15. Initialise and update and git submodules.
-# 16. Move /trunk/assets up to /assets.
-# 17. Move into /trunk, and SVN commit.
-# 18. Move into /assets, and SVN commit.
-# 19. Copy /trunk into /tags/{version}, and SVN commit.
-# 20. Delete temporary local SVN checkout.
+# A modification of Dean Clatworthy's deploy script as found here: https://github.com/deanc/wordpress-plugin-git-svn
+# The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
 
-echo
-echo "WordPress Plugin SVN Deploy v3.0.0"
-echo
-echo "Let's collect some information first. There are six questions."
-echo
-echo "Default values are in brackets - just hit enter to accept them."
-echo
+# main config
+PLUGINSLUG="imageboss"
+CURRENTDIR=`pwd`
+MAINFILE="image-boss.php" # this should be the name of your main php file in the wordpress plugin
 
-# Get some user input
-# Can't use the -i flag for read, since that doesn't work for bash 3
-printf "Q1. WordPress Repo Plugin Slug e.g. my-awesome-plugin: "
-read -e PLUGINSLUG
-echo
+# git config
+GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 
-# Set up some default values. Feel free to change these in your own script
-CURRENTDIR=$(pwd)
-default_svnpath="/tmp/$PLUGINSLUG"
-default_svnurl="https://plugins.svn.wordpress.org/$PLUGINSLUG"
-default_svnuser="GaryJ"
-default_plugindir="$CURRENTDIR/$PLUGINSLUG"
-default_mainfile="$PLUGINSLUG.php"
+# svn config
+SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
+SVNURL="http://plugins.svn.wordpress.org/$PLUGINSLUG/" # Remote SVN repo on wordpress.org, with no trailing slash
+SVNUSER="igorescobar" # your svn username
 
-echo "Q2. Your local plugin root directory (the Git repo)."
-printf "($default_plugindir): "
-read -e  input
-input="${input%/}" # Strip trailing slash
-PLUGINDIR="${input:-$default_plugindir}" # Populate with default if empty
-echo
-
-# Check directory exists.
-if [ ! -d "$PLUGINDIR" ]; then
-  echo "Directory $PLUGINDIR not found. Aborting."
-  exit 1;
-fi
-
-printf "Q3. Name of the main plugin file ($default_mainfile): "
-read -e input
-MAINFILE="${input:-$default_mainfile}" # Populate with default if empty
-echo
-
-# Check main plugin file exists.
-if [ ! -f "$PLUGINDIR/$MAINFILE" ]; then
-  echo "Plugin file $PLUGINDIR/$MAINFILE not found. Aborting."
-  exit 1;
-fi
-
-echo "Checking version in main plugin file matches version in readme.txt file..."
-echo
-
-# Check version in readme.txt is the same as plugin file after translating both to Unix line breaks to work around grep's failure to identify Mac line breaks
-PLUGINVERSION=$(grep -i "Version:" $PLUGINDIR/$MAINFILE | awk -F' ' '{print $NF}' | tr -d '\r')
-echo "$MAINFILE version: $PLUGINVERSION"
-READMEVERSION=$(grep -i "Stable tag:" $PLUGINDIR/readme.txt | awk -F' ' '{print $NF}' | tr -d '\r')
-echo "readme.txt version: $READMEVERSION"
-
-if [ "$READMEVERSION" = "trunk" ]; then
-	echo "Version in readme.txt & $MAINFILE don't match, but Stable tag is trunk. Let's continue..."
-elif [ "$PLUGINVERSION" != "$READMEVERSION" ]; then
-	echo "Version in readme.txt & $MAINFILE don't match. Exiting...."
-	exit 1;
-elif [ "$PLUGINVERSION" = "$READMEVERSION" ]; then
-	echo "Versions match in readme.txt and $MAINFILE. Let's continue..."
-fi
-
-echo
-
-echo "Q4. Path to a local directory where a temporary SVN checkout can be made."
-printf "Don't add trunk ($default_svnpath): "
-read -e input
-input="${input%/}" # Strip trailing slash
-SVNPATH="${input:-$default_svnpath}" # Populate with default if empty
-echo
-
-echo "Q5. Remote SVN repo on WordPress.org."
-printf "($default_svnurl): "
-read -e input
-input="${input%/}" # Strip trailing slash
-SVNURL="${input:-$default_svnurl}" # Populate with default if empty
-echo
-
-printf "Q6. Your WordPress repo SVN username ($default_svnuser): "
-read -e input
-SVNUSER="${input:-$default_svnuser}" # Populate with default if empty
-echo
-
-echo "That's all of the data collected."
-echo
-echo "Slug: $PLUGINSLUG"
-echo "Plugin directory: $PLUGINDIR"
-echo "Main file: $MAINFILE"
-echo "Temp checkout path: $SVNPATH"
-echo "Remote SVN repo: $SVNURL"
-echo "SVN username: $SVNUSER"
-echo
-
-printf "OK to proceed (Y|n)? "
-read -e input
-PROCEED="${input:-y}"
-echo
-
-# Allow user cancellation
-if [ $(echo "$PROCEED" |tr [:upper:] [:lower:]) != "y" ]; then echo "Aborting..."; exit 1; fi
 
 # Let's begin...
 echo ".........................................."
 echo
-echo "Preparing to deploy WordPress plugin"
+echo "Preparing to deploy wordpress plugin"
 echo
 echo ".........................................."
 echo
 
-echo
+# Check version in readme.txt is the same as plugin file after translating both to unix line breaks to work around grep's failure to identify mac line breaks
+NEWVERSION1=`grep "^Stable tag:" $GITPATH/readme.txt | awk -F' ' '{print $NF}'`
+echo "readme.txt version: $NEWVERSION1"
+echo "$GITPATH$MAINFILE"
+NEWVERSION2=`grep "Version:" $GITPATH$MAINFILE | awk -F' ' '{print $NF}'`
+echo "$MAINFILE version: $NEWVERSION2"
 
-echo "Changing to $PLUGINDIR"
-cd $PLUGINDIR
+if [ "$NEWVERSION1" -ne "$NEWVERSION2" ]; then echo "Version in readme.txt & $MAINFILE don't match. Exiting...."; exit 1; fi
 
-# Check for git tag (may need to allow for leading "v"?)
-# if git show-ref --tags --quiet --verify -- "refs/tags/$PLUGINVERSION"
-if git show-ref --tags --quiet --verify -- "refs/tags/$PLUGINVERSION"
-	then
-		echo "Git tag $PLUGINVERSION does exist. Let's continue..."
-	else
-		echo "$PLUGINVERSION does not exist as a git tag. Aborting.";
+echo "Versions match in readme.txt and $MAINFILE. Let's proceed..."
+
+if git show-ref --tags --quiet --verify -- "refs/tags/$NEWVERSION1"
+    then
+		echo "Version $NEWVERSION1 already exists as git tag. Exiting....";
 		exit 1;
+	else
+		echo "Git version does not exist. Let's proceed..."
 fi
 
+cd $GITPATH
+echo -e "Enter a commit message for this new version: \c"
+read COMMITMSG
+git commit -am "$COMMITMSG"
+
+echo "Tagging new version in git"
+git tag -a "$NEWVERSION1" -m "Tagging version $NEWVERSION1"
+
+echo "Pushing latest commit to origin, with tags"
+git push origin master
+git push origin master --tags
+
 echo
-
-echo "Creating local copy of SVN repo trunk..."
-svn checkout $SVNURL $SVNPATH --depth immediates
-svn update --quiet $SVNPATH/trunk --set-depth infinity
-
-echo "Ignoring GitHub specific files"
-svn propset svn:ignore "README.md
-Thumbs.db
-.github/*
-.git
-.gitattributes
-.gitignore" "$SVNPATH/trunk/"
+echo "Creating local copy of SVN repo ..."
+svn co $SVNURL $SVNPATH
 
 echo "Exporting the HEAD of master from git to the trunk of SVN"
 git checkout-index -a -f --prefix=$SVNPATH/trunk/
 
-# If submodule exist, recursively check out their indexes
-if [ -f ".gitmodules" ]
-	then
-		echo "Exporting the HEAD of each submodule from git to the trunk of SVN"
-		git submodule init
-		git submodule update
-		git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
-			while read path_key path
-			do
-				#url_key=$(echo $path_key | sed 's/\.path/.url/')
-				#url=$(git config -f .gitmodules --get "$url_key")
-				#git submodule add $url $path
-				echo "This is the submodule path: $path"
-				echo "The following line is the command to checkout the submodule."
-				echo "git submodule foreach --recursive 'git checkout-index -a -f --prefix=$SVNPATH/trunk/$path/'"
-				git submodule foreach --recursive 'git checkout-index -a -f --prefix=$SVNPATH/trunk/$path/'
-			done
-fi
+echo "Ignoring github specific files and deployment script"
+svn propset svn:ignore "deploy.sh
+README.md
+.git
+.gitignore" "$SVNPATH/trunk/"
 
-echo
-
-# Support for the /assets folder on the .org repo.
-echo "Moving assets."
-# Make the directory if it doesn't already exist
-mkdir -p $SVNPATH/assets/
-mv $SVNPATH/trunk/assets/* $SVNPATH/assets/
-svn add --force $SVNPATH/assets/
-svn delete --force $SVNPATH/trunk/assets
-
-echo
-
-echo "Changing directory to SVN and committing to trunk."
+echo "Changing directory to SVN and committing to trunk"
 cd $SVNPATH/trunk/
-# Delete all files that should not now be added.
-svn status | grep -v "^.[ \t]*\..*" | grep "^\!" | awk '{print $2"@"}' | xargs svn del
 # Add all new files that are not set to be ignored
-svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2"@"}' | xargs svn add
-svn commit --username=$SVNUSER -m "Preparing for $PLUGINVERSION release"
+svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
+svn commit --username=$SVNUSER -m "$COMMITMSG"
 
-echo
-
-echo "Updating WordPress plugin repo assets and committing."
-cd $SVNPATH/assets/
-# Delete all new files that are not set to be ignored
-svn status | grep -v "^.[ \t]*\..*" | grep "^\!" | awk '{print $2"@"}' | xargs svn del
-# Add all new files that are not set to be ignored
-svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2"@"}' | xargs svn add
-svn update --quiet --accept working $SVNPATH/assets/*
-svn commit --username=$SVNUSER -m "Updating assets"
-
-echo
-
-echo "Creating new SVN tag and committing it."
+echo "Creating new SVN tag & committing it"
 cd $SVNPATH
-svn copy --quiet trunk/ tags/$PLUGINVERSION/
-# Remove assets and trunk directories from tag directory
-svn delete --force --quiet $SVNPATH/tags/$PLUGINVERSION/assets
-svn delete --force --quiet $SVNPATH/tags/$PLUGINVERSION/trunk
-svn update --quiet --accept working $SVNPATH/tags/$PLUGINVERSION
-cd $SVNPATH/tags/$PLUGINVERSION
-svn commit --username=$SVNUSER -m "Tagging version $PLUGINVERSION"
+svn copy trunk/ tags/$NEWVERSION1/
+cd $SVNPATH/tags/$NEWVERSION1
+svn commit --username=$SVNUSER -m "Tagging version $NEWVERSION1"
 
-echo
-
-echo "Removing temporary directory $SVNPATH."
-cd $SVNPATH
-cd ..
+echo "Removing temporary directory $SVNPATH"
 rm -fr $SVNPATH/
 
 echo "*** FIN ***"
--- imageboss-plugin/readme.txt
+++ imageboss-plugin/readme.txt
@@ -3,7 +3,7 @@ Contributors: igorescobar
 Tags: cdn, images, compression, face detection, progressive scans, watermarking
 Requires at least: 3.0
 Tested up to: 4.9
-Stable tag: 1.0.4
+Stable tag: 1.0.5
 License: MIT
 License URI: https://opensource.org/licenses/MIT
 
