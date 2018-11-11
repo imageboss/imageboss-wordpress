88d5c95f47d2ce4d925ca04a849d141aa737c4c8
Igor Escobar <igor.escobar-ext@man.eu>
2018-11-11 21:32:26 +0000

    Adding settings links to the plugins home

---
+++ admin/includes/plugin-actionlinks.php
@@ -0,0 +1,11 @@
+<?php
+
+add_filter('plugin_action_links', 'ibup_plugin_admin_action_links', 10, 2);
+
+function ibup_plugin_admin_action_links($links, $file) {
+  if ($file === IBUP_BASENAME) {
+    $settings_link = '<a href="admin.php?page=imageboss-setting">Settings</a>';
+    array_unshift($links, $settings_link);
+  }
+  return $links;
+}
