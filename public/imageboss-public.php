0bc9971d5daea2fa0d4fa661eb57b1fc29f88476
Igor Escobar <igor.escobar-ext@man.eu>
2018-11-13 09:56:28 +0000

    adding a low priority to the method who replaces the srcs

--- public/imageboss-public.php
+++ public/imageboss-public.php
@@ -5,13 +5,13 @@ function ibup_buffer_callback($buffer) {
   return ibup_apply_imageboss_urls($buffer);
 }
 
-add_action('wp_head', 'ibup_buffer_start');
+add_action('wp_head', 'ibup_buffer_start', 999);
 
 function ibup_buffer_start() {
   ob_start("ibup_buffer_callback");
 }
 
-add_action('wp_footer', 'ibup_buffer_end');
+add_action('wp_footer', 'ibup_buffer_end', 999);
 function ibup_buffer_end() {
   ob_end_flush();
 }
