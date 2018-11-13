17ef628d41a39b6a56ad00a5abbc6c189919a2fe
Igor Escobar <igor.escobar-ext@man.eu>
2018-11-13 08:53:36 +0000

    Improving logic that generates the tag img inside of tinymce editor

--- admin/includes/add-media-widget-customization.php
+++ admin/includes/add-media-widget-customization.php
@@ -1,14 +1,15 @@
 <?php
 
 add_filter('attachment_fields_to_edit', 'ibup_add_media_custom_field', 10, 2);
-function ibup_add_media_custom_field($form_fields, $post) {
+function ibup_add_media_custom_field($form_fields, $post)
+{
   $form_fields['imageBoss-configuration'] = array(
-      'label' => 'ImageBoss Configuration',
+    'label' => 'ImageBoss Configuration',
   );
 
   $form_fields['operation'] = array(
-      'label' => 'Operation',
-      'input' => 'html',
+    'label' => 'Operation',
+    'input' => 'html',
   );
 
   $form_fields['operation']['html'] = '<select name="operation" id="operation">';
@@ -21,13 +22,13 @@ function ibup_add_media_custom_field($form_fields, $post) {
 
   $imageboss_cover_mode = get_post_meta($post->ID, 'imageboss-cover-mode', true);
   $form_fields['imageboss-cover-mode'] = array(
-      'label' => __('Cover Mode'),
-      'input' => 'hidden',
+    'label' => __('Cover Mode'),
+    'input' => 'hidden',
   );
 
   $form_fields['cover_mode'] = array(
-      'label' => '',
-      'input' => 'html',
+    'label' => '',
+    'input' => 'html',
   );
 
   $form_fields['cover_mode']['html'] = '<select id="blue" style="display:none;" name="cover_mode">';
@@ -49,28 +50,28 @@ function ibup_add_media_custom_field($form_fields, $post) {
 
   $imageboss_width = get_post_meta($post->ID, 'imageboss-width', true);
   $form_fields['imageboss-width'] = array(
-      'label' => __('Width'),
-      'input' => 'text',
-      'value' => '',
+    'label' => __('Width'),
+    'input' => 'text',
+    'value' => '',
   );
 
   $imageboss_height = get_post_meta($post->ID, 'imageboss-height', true);
   $form_fields['imageboss-height'] = array(
-      'label' => __('Height'),
-      'input' => 'text',
-      'value' => '',
+    'label' => __('Height'),
+    'input' => 'text',
+    'value' => '',
   );
   $imageboss_opt = get_post_meta($post->ID, 'imageboss-operation', true);
   $form_fields['imageboss-operation'] = array(
-      'label' => __('Operation'),
-      'input' => 'hidden',
-      'value' => '',
+    'label' => __('Operation'),
+    'input' => 'hidden',
+    'value' => '',
   );
   $imageboss_options = get_post_meta($post->ID, 'imageboss-options', true);
   $form_fields['imageboss-options'] = array(
-      'label' => __('Options'),
-      'input' => 'text',
-      'value' => '',
+    'label' => __('Options'),
+    'input' => 'text',
+    'value' => '',
   );
   return $form_fields;
 }
@@ -102,86 +103,79 @@ add_filter('image_send_to_editor', 'ibup_custom_html_template', 1, 8);
 function ibup_custom_html_template($html, $id, $caption, $title, $align, $url, $size, $alt)
 {
 
-    list($img_src, $width, $height) = image_downsize($id, $size);
-    $hwstring = image_hwstring($width, $height);
+  list($img_src, $width, $height) = image_downsize($id, $size);
+  $original_image = wp_get_attachment_image_src($id, 'full');
 
-    $image_boss = wp_get_attachment_image_src($id, 'full');
+  $image = new DOMDocument();
+  $image->loadXML($html);
+  $imgs = $image->getElementsByTagName('img');
 
-    if ($url) {
-        $out .= '<a href="' . $url . '" class="fancybox">';
-    }
-
+  foreach ($imgs as $img) {
     $field_operation = get_post_meta($id, 'imageboss-operation', true);
+    $field_cover_mode = get_post_meta($id, 'imageboss-cover-mode', true);
+    $field_width = get_post_meta($id, 'imageboss-width', true);
+    $field_height = get_post_meta($id, 'imageboss-height', true);
+    $field_options = get_post_meta($id, 'imageboss-options', true);
 
-    if ($field_operation == 'cover') {
-        $field_width = get_post_meta($id, 'imageboss-width', true);
-        $field_height = get_post_meta($id, 'imageboss-height', true);
-        $field_cover_mode = get_post_meta($id, 'imageboss-cover-mode', true);
-        $field_options = get_post_meta($id, 'imageboss-options', true);
-        $abd = 'cover-mode="' . $field_cover_mode . '"';
-
-        $out .= '<img src="' . $image_boss[0] . '" alt="' . $alt . '" imageboss-operation="' . $field_operation . '" ' . $abd . ' imageboss-width="' . $field_width . '" imageboss-height="' . $field_height . '" imageboss-options="' . $field_options . '"/>';
-
-    } elseif ($field_operation == 'width') {
-
-        $field_width = get_post_meta($id, 'imageboss-width', true);
-        $field_options = get_post_meta($id, 'imageboss-options', true);
-        $out .= '<img src="' . $image_boss[0] . '" alt="' . $alt . '" imageboss-operation="' . $field_operation . '" imageboss-width="' . $field_width . '" imageboss-options="' . $field_options . '"/>';
-
-    } elseif ($field_operation == 'height') {
-        $field_options = get_post_meta($id, 'imageboss-options', true);
-        $field_height = get_post_meta($id, 'imageboss-height', true);
-        $out .= '<img src="' . $image_boss[0] . '" alt="' . $alt . '" imageboss-operation="' . $field_operation . '" imageboss-height="' . $field_height . '" imageboss-options="' . $field_options . '"/>';
-    } elseif ($field_operation == 'cdn') {
-        $field_options = get_post_meta($id, 'imageboss-options', true);
-        $field_operation = get_post_meta($id, 'imageboss-operation', true);
-
-        $out .= '<img src="' . $image_boss[0] . '" alt="' . $alt . '" imageboss-operation="' . $field_operation . '" imageboss-options="' . $field_options . '" />';
-    } else {
-        $field_options = get_post_meta($id, 'imageboss-options', true);
-        $field_operation = get_post_meta($id, 'imageboss-operation', true);
-        $out .= '<img src="' . $image_boss[0] . '" alt="' . $alt . '" imageboss-options="' . $field_options . '" />';
+    if ($field_operation) {
+      // only when the user selects a operation we should use the original image
+      $img->setAttribute('src', $original_image[0]);
+      $img->setAttribute('imageboss-operation', $field_operation);
     }
 
-    if ($url) {
-        $out .= '</a>';
+    if ($field_cover_mode) {
+      $img->setAttribute('imageboss-cover-mode', $field_cover_mode);
     }
 
-    return $out; // the result HTML
+    if ($field_width) {
+      $img->setAttribute('imageboss-width', $field_width);
+    }
+
+    if ($field_height) {
+      $img->setAttribute('imageboss-height', $field_height);
+    }
+
+    if ($field_options) {
+      $img->setAttribute('imageboss-options', $field_options);
+    }
+
+  }
+
+  return $image->saveHTML();
 }
 
 add_filter('tiny_mce_before_init', 'ibup_override_mce_options');
 function ibup_override_mce_options($initArray)
 {
-    $opts = '*[*]';
-    $initArray['valid_elements'] = $opts;
-    $initArray['extended_valid_elements'] = $opts;
-    return $initArray;
+  $opts = '*[*]';
+  $initArray['valid_elements'] = $opts;
+  $initArray['extended_valid_elements'] = $opts;
+  return $initArray;
 }
 
 add_action('media_buttons', 'ibup_reset_attribute_of_images');
 function ibup_reset_attribute_of_images()
 {
-    $query_images_args = array(
-        'post_type' => 'attachment',
-        'post_mime_type' => 'image',
-        'post_status' => 'inherit',
-        'posts_per_page' => -1,
-    );
+  $query_images_args = array(
+    'post_type' => 'attachment',
+    'post_mime_type' => 'image',
+    'post_status' => 'inherit',
+    'posts_per_page' => -1,
+  );
 
-    $query_images = new WP_Query($query_images_args);
+  $query_images = new WP_Query($query_images_args);
 
-    $images = array();
-    foreach ($query_images->posts as $image) {
-        $images_id = $image->ID;
-        $myvals = get_post_meta($images_id);
-        foreach ($myvals as $key => $val) {
-            foreach ($val as $vals) {
-                if (($key == 'imageboss-operation') || ($key == 'imageboss-width') || ($key == 'imageboss-height') || ($key == 'imageboss-cover-mode') || ($key == 'imageboss-options')) {
-                    update_post_meta($images_id, $key, '');
-                }
-            }
+  $images = array();
+  foreach ($query_images->posts as $image) {
+    $images_id = $image->ID;
+    $myvals = get_post_meta($images_id);
+    foreach ($myvals as $key => $val) {
+      foreach ($val as $vals) {
+        if (($key == 'imageboss-operation') || ($key == 'imageboss-width') || ($key == 'imageboss-height') || ($key == 'imageboss-cover-mode') || ($key == 'imageboss-options')) {
+          update_post_meta($images_id, $key, '');
         }
+      }
     }
+  }
 
 }
